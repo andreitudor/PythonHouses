2d51251417b0187b31b95305cdd2221c1d912de7
Andrei Tudor <andrei.tudor88@gmail.com>
2020-06-01 12:17:16 +0000

    Add fixes to algo

--- classes/city.py
+++ classes/city.py
@@ -2,45 +2,30 @@ from .house import House
 
 class City:
   def __init__(self):
-    self.first = House(0)
-    self.last = House(0)
-    self.first.rightNeighbor = self.last
-    self.last.leftNeighbor = self.first
-    #set them to 0 as they never change
-    self.first.beforeDay = 0
-    self.last.beforeDay = 0
-    self.size = 2
+    self.houses = []
+    #append first and last houses
+    self.houses.append(House(0))
+    self.houses[0].beforeDay = 0
+    self.houses.append(House(0))
+    self.houses[1].beforeDay = 0
 
   def buyHouse(self, day):
-    house = House(day)
-    beforeLast = self.last.leftNeighbor
-    house.leftNeighbor = beforeLast
-    house.rightNeighbor = self.last
-    beforeLast.rightNeighbor = house
-    self.last.leftNeighbor = house
-    self.size += 1
+    lastHouse = self.houses.pop()
+    self.houses.append(House(day))
+    self.houses.append(lastHouse)
 
   def inorder(self):
-    self._inorder(self.first)
+    states = []
+    for i in range(1,len(self.houses) - 1):
+      states.append(self.houses[i].day)
+    return states
 
-  def _inorder(self, house):
-    print(house.day,  end = " ")
-    if house.leftNeighbor is not None:
-      print("link left", house.leftNeighbor.day, end = " ")
-    if house.rightNeighbor is not None:
-      print("link right", house.rightNeighbor.day, end = " ")
-      print()
-      self._inorder(house.rightNeighbor)
-  
   def checkActivity(self):
-    #skip first neighbor as always 0
-    self._checkActivity(self.first.rightNeighbor)
-  
-  def _checkActivity(self, node):
-    if node.leftNeighbor.beforeDay == node.rightNeighbor.day:
-      node.day = 0
-    else:
-      node.day = 1
-    if node.rightNeighbor is not None:
-      self._checkActivity(node.rightNeighbor)
-      
+    #skip first and last houses
+    for i in range(1,len(self.houses)-1):
+      if self.houses[i-1].beforeDay == self.houses[i+1].day:
+        self.houses[i].beforeDay = self.houses[i].day
+        self.houses[i].day = 0
+      else:
+        self.houses[i].beforeDay = self.houses[i].day
+        self.houses[i].day = 1
--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from classes.city import City
 
 list1 = [1, 0, 0, 0, 0, 1, 0, 0]
-list2 = []
+list2 = [1, 1, 1, 0, 1, 1, 1, 1]
 
 city = City()
 
 for state in list1:
   city.buyHouse(state)
+print(city.inorder())
+for i in range(1, 2):
+  city.checkActivity()
 
-city.inorder()
+print(city.inorder())
+
+city2 = City()
+
+for state in list2:
+  city2.buyHouse(state)
+print(city2.inorder())
+for i in range(1, 3):
+  city2.checkActivity()
+
+print(city2.inorder())
